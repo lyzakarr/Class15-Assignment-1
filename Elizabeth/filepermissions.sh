49a33c02678f144be8f68624dd453bcfdc71edeb
Elizabeth Fobid <lyzakarr@yhoo.co.uk>
2023-03-08 17:12:26 +0000

    conditional statements to file existence

--- Elizabeth/filepermissions.sh
+++ Elizabeth/filepermissions.sh
@@ -9,4 +9,19 @@ filename=$1
 # Assigning $1 as variable to the first arguement in the script, being the filename
 
 
+if [ -f $1 ]
+
+then
+        echo " The file exists and it's a regular file "
+
+        cp $filename $filename.$(date +%F)
+
+        echo "'$(ls -la $filename)'"
+
+else
+
+        echo "$filename does not exist"
+
+# Conditional Statements(if and else)that determins the exectuion of another action if the file exists or not.
+
 
