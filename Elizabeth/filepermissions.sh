9499446bfb51a1dd4ca698e96ff25694182d5bf9
Elizabeth Fobid <lyzakarr@yhoo.co.uk>
2023-03-08 16:53:35 +0000

    assigning variable to arguement

--- Elizabeth/filepermissions.sh
+++ Elizabeth/filepermissions.sh
@@ -2,4 +2,11 @@
 
 # The shebang, being the script interpreter
 
-# Determine whether file exists in the system or not
+# Determine whether file exists in the directory or not
+
+filename=$1
+
+# Assigning $1 as variable to the first arguement in the script, being the filename
+
+
+
